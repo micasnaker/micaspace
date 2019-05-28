ae94469e47cbec42ca3f680106051a9c94e4a420
wangJ <micasnaker@gmail.com>
2019-05-28 16:20:46 +0800

    添加页面右下角版黑猫

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,9 @@
 <template>
   <div id="app">
     <router-view></router-view>
+    <div class="black_cat">
+      <iframe src="https://zhanyuzhang.github.io/lovely-cat/cat.html" border="0"></iframe>
+    </div>
     <music v-if="$route.meta.isShowMusic"></music>
   </div>
 </template>
@@ -19,5 +22,29 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
+.black_cat {
+  position: fixed;
+  right: 360px;
+  top: 590px;
+  z-index: 999;
+  iframe {
+    border: none !important;
+  }
+}
+</style>
+
+<style lang="scss" scoped>
+@media (max-width: 420px) {
+  //mobile
+  .black_cat {
+    position: fixed;
+    top: 230px;
+    right: 0;
+    z-index: 9999;
+    iframe {
+      border: none !important;
+    }
+  }
+}
 </style>
